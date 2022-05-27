ede77d297f2441001e46eab02e9dbf114932f2f2
Dave McFarland <dmcfarlandjr@gmail.com>
2022-05-27 09:56:35 -0700

    Updated column printing to work if there are less than three words.

--- wordle_helper.py
+++ wordle_helper.py
@@ -1,4 +1,5 @@
 import json, difflib
+import cmd
 from pprint import pprint as pp
 
 
@@ -8,10 +9,10 @@ class WordleHelper:
 
         defaults = {
             'words': self.load_words(),
-            'word_pattern': '--i--',
-            'exclude_pattern': 'n--e-',
-            'includes': ['n', 'i', 'e'],
-            'excludes': ['o', 't', 'h', 'r', 'a', 'l', 's'],
+            'word_pattern': 't-a--',
+            'exclude_pattern': '-r-i-',
+            'includes': ['a', 'i', 't', 'r'],
+            'excludes': ['n', 'l', 's', 'o', 'h', 'e'],
             'filtered_word_list': []
         }
 
@@ -32,7 +33,8 @@ class WordleHelper:
         print(self.settings_string)
 
     @staticmethod
-    def load_words(source='../../resources/json/words_dictionary.json', as_list=True):
+    def load_words(source='../../resources/json/wordle_words.json', as_list=True):
+
         with open(source, 'r') as word_file:
             valid_words = json.loads(word_file.read())
 
@@ -44,11 +46,10 @@ class WordleHelper:
         return [word for word in self.words if len(word) == 5]
 
     def filter_five_letter_words(self):
-
         # This flow control is intentionally verbose to make the logic easier to read and understand.
         for word in self.words:
             matches_exclude_pattern = any([c for idx, c in enumerate([*self.exclude_pattern]) if word[idx] == self.exclude_pattern[idx]])
-
+            
             if not matches_exclude_pattern:
                 matches_word_pattern = any([c for idx, c in enumerate([*self.word_pattern]) if word[idx] == self.word_pattern[idx]])
 
@@ -69,10 +70,14 @@ def column_print(data):
     for a, b, c in zip(data[::3], data[1::3], data[2::3]):
         print('{:<30}{:<30}{:<}'.format(a, b, c))
 
+def column_print_cli(data):
+    cli = cmd.Cmd()
+    cli.columnize(data, displaywidth=80)
 
 if __name__ == '__main__':
     helper = WordleHelper()
     filtered_fives = helper.filter_five_letter_words()
 
     print("\nPossible matches:")
-    column_print(filtered_fives)
+    # column_print(filtered_fives)
+    column_print_cli(filtered_fives)
