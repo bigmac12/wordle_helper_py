0336bada99e5890fd9a4d70e8dcda47aff2172f1
Dave McFarland <dmcfarlandjr@gmail.com>
2022-05-27 12:59:48 -0700

    Removed unnecessary import. Did some refactoring to better encapsulate the methods of the helper class.

--- wordle_helper.py
+++ wordle_helper.py
@@ -1,4 +1,4 @@
-import json, difflib
+import json
 import cmd
 from pprint import pprint as pp
 
@@ -6,6 +6,7 @@ from pprint import pprint as pp
 class WordleHelper:
     def __init__(self):
         self.DEBUG = True
+        self.display_width = 80
 
         defaults = {
             'words': self.load_words(),
@@ -20,6 +21,7 @@ class WordleHelper:
 
         for key, value in defaults.items():
             setattr(self, key, defaults.get(key, value))
+
             if key not in ['words', 'filtered_word_list']:
                 key_str = "{}:".format(key).ljust(19, ' ')
                 self.settings_string += "{key_str} {value}\n".format(key_str=key_str, value=value)
@@ -27,14 +29,20 @@ class WordleHelper:
         if self.DEBUG:
             self.__print_settings()
 
+    def __print_heading(self, heading):
+        print("\n{heading}".format(heading=heading))
+        print(''.join(['-' for char in heading]))
+
     def __print_settings(self):
-        print("\nCurrent settings")
-        print("----------------")
+        self.__print_heading("Current settings")
         print(self.settings_string)
+
+    def __column_print(self, data):
+        cli = cmd.Cmd()
+        cli.columnize(data, displaywidth=self.display_width)
 
     @staticmethod
     def load_words(source='../../resources/json/wordle_words.json', as_list=True):
-
         with open(source, 'r') as word_file:
             valid_words = json.loads(word_file.read())
 
@@ -48,37 +56,33 @@ class WordleHelper:
     def filter_five_letter_words(self):
         # This flow control is intentionally verbose to make the logic easier to read and understand.
         for word in self.words:
-            matches_exclude_pattern = any([c for idx, c in enumerate([*self.exclude_pattern]) if word[idx] == self.exclude_pattern[idx]])
+            matches_exclude_pattern = any([char for idx, char in enumerate([*self.exclude_pattern]) if word[idx] == self.exclude_pattern[idx]])
             
             if not matches_exclude_pattern:
-                matches_word_pattern = any([c for idx, c in enumerate([*self.word_pattern]) if word[idx] == self.word_pattern[idx]])
+                matches_word_pattern = any([char for idx, char in enumerate([*self.word_pattern]) if word[idx] == self.word_pattern[idx]])
 
                 if matches_word_pattern or self.word_pattern == '-----':
-                    has_correct_includes = all([c in word for c in self.includes])
+                    has_correct_includes = all([char in word for char in self.includes])
 
                     if has_correct_includes:
-                        contains_excludes = any([c in word for c in self.excludes])
+                        contains_excludes = any([char in word for char in self.excludes])
                 
                         if not contains_excludes:
                             self.filtered_word_list.append(word)
 
-
         return self.filtered_word_list
+
+    def print_results(self):
+        self.__print_heading("Possible matches")
+        self.__column_print(self.filter_five_letter_words())
 
 
 @DeprecationWarning
-def column_print(data):
+def column_print_old(data):
     for a, b, c in zip(data[::3], data[1::3], data[2::3]):
         print('{:<30}{:<30}{:<}'.format(a, b, c))
 
-def column_print_cli(data):
-    cli = cmd.Cmd()
-    cli.columnize(data, displaywidth=80)
 
 if __name__ == '__main__':
     helper = WordleHelper()
-    filtered_fives = helper.filter_five_letter_words()
-
-    print("\nPossible matches:")
-    # column_print(filtered_fives)
-    column_print_cli(filtered_fives)
+    helper.print_results()
